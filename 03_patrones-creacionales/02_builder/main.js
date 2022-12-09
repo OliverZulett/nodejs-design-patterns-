848911d975ab40ce56e21392ced2ea6808ce7ac1
oliver zulett <oliverzulett@gmail.com>
2022-12-08 21:39:37 -0400

    url builder

--- 03_patrones-creacionales/02_builder/main.js
+++ 03_patrones-creacionales/02_builder/main.js
@@ -1,13 +1,20 @@
 import CarBuilder from "./car.builder.js";
+import UrlBuilder from "./url.builder.js";
 
 const mySedanCar = new CarBuilder()
-  .engine('in line', 'mercedez', 'MXZ2024')
-  .suspension('hybrid')
+  .engine("in line", "mercedez", "MXZ2024")
+  .suspension("hybrid")
   .doors(4)
   .sits(5)
-  .withAirbags(6, 'mercedez', 'nylon')
-  .withSpareTire('firestone')
-  .chassisColor('gray')
+  .withAirbags(6, "mercedez", "nylon")
+  .withSpareTire("firestone")
+  .chassisColor("gray")
   .build();
 
-console.log(mySedanCar);
+const url = new UrlBuilder()
+  .setProtocol("https")
+  .setAuthentication("user", "password")
+  .setHostname("www.myhost.com")
+  .build();
+
+console.log(url.toString());
